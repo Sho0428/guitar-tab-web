c83cc2199e61a85ff939584d819f2745e597a457
Sho0428 <f23aa006@chuo.ac.jp>
2025-12-04 11:51:40 +0900

    Refactor AudioProcessor to handle input array
    Updated input handling to support structured cloning.

--- processor.js
+++ processor.js
@@ -1,8 +1,9 @@
 class AudioProcessor extends AudioWorkletProcessor {
   process(inputs, outputs, parameters) {
-    const input = inputs[0][0];
-    if (input) {
-      this.port.postMessage(input);
+    const input = inputs[0];
+    if (input && input[0]) {
+       // postMessage は構造化クローンを使うので TypedArray を渡しても OK
+      this.port.postMessage(input[0]);
     }
     return true;
   }
